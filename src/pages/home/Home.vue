6bc27eebc8fc42a612bba3461a7f4d48e2eccbc1
jinyuhuang <1099762747@qq.com>
2018-09-30 16:52:38 +0800

    init HomeIcon

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,20 @@
   <div>
     <home-header></home-header>
     <Swiper></Swiper>
+    <Icons></Icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import Swiper from './components/Swiper'
+import Icons from './components/Icons'
 export default {
+  name: 'Home',
   components: {
     HomeHeader,
-    Swiper
+    Swiper,
+    Icons
   }
 }
 </script>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+    <div class="icons"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper
+    padding-bottom: 50%
+    background: pink
+    .icons
+      width: 25%
+      padding-bottom: 25%
+      float: left
+      background: #a1c358
+</style>
