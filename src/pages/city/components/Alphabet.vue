04a6738d4810ca2639ef54f8ef6bf1b97d5933a7
jinyuhuang <1099762747@qq.com>
2018-10-11 14:36:05 +0800

    city-list page

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="list">
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+    >{{item}}</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varible.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
