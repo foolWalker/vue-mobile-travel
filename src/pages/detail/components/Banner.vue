ce43741cd419a4f8d3f04a6f13e706d2b6a3c34c
jinyuhuang <1099762747@qq.com>
2018-10-18 15:53:04 +0800

    detail-page

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1504/47/4730a0c01664b63d.water.jpg_600x330_3bf89257.jpg" />
+      <img class="banner-img" :src="bannerImg" />
       <div class="banner-info">
-        <div class="banner-tittle">武汉海昌极地海洋世界</div>
+        <div class="banner-tittle">{{sightName}}</div>
         <div class="banner-number">
           <span class="iconfont icon-picture banner-icon"></span>
-          1
+          {{bannerImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallary
-      v-show="showGallary"
-      :imgs="imgs"
-      @close="handleGallaryClose"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        v-show="showGallary"
+        :imgs="bannerImgs"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
   },
   data () {
     return {
-      imgs: ['http://img1.qunarzz.com/sight/p0/1504/47/4730a0c01664b63d.water.jpg_600x330_3bf89257.jpg', 'http://img1.qunarzz.com/sight/p0/1504/47/4730a0c01664b63d.water.jpg_600x330_3bf89257.jpg'],
       showGallary: false
     }
   },
@@ -45,33 +51,45 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .banner
-    position: relative
-    overflow: hidden
-    height: 0
-    padding-bottom: 55%
-    .banner-img
-      width: 100%
-    .banner-info
-      display: flex
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      line-height: .6rem
-      color: #fff
-      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
-      .banner-tittle
-        flex: 1
-        font-size: .32rem
-        padding: 0 .2rem
-      .banner-number
-        height: .32rem
-        line-height: .32rem
-        margin-top: .14rem
-        padding: 0 .4rem
-        border-radius: .2rem
-        background: rgba(0, 0, 0, .8)
-        .banner-icon
-          font-size: .3rem
+.banner {
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+
+  .banner-img {
+    width: 100%;
+  }
+
+  .banner-info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: 0.6rem;
+    color: #fff;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+
+    .banner-tittle {
+      flex: 1;
+      font-size: 0.32rem;
+      padding: 0 0.2rem;
+    }
+
+    .banner-number {
+      height: 0.4rem;
+      line-height: 0.4rem;
+      margin-top: 0.14rem;
+      padding: 0 0.4rem;
+      border-radius: 0.2rem;
+      background: rgba(0, 0, 0, 0.8);
+      font-size: 0.3 0.4rem;
+
+      .banner-icon {
+        font-size: 0.3 0.4rem;
+      }
+    }
+  }
+}
 </style>
